4e85060c010dca8f9ea4cd900b8df7bb5a00edd2
Ankit Grin <ankit@softradix.com>
2022-08-26 12:50:12 +0530

    authentication done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import { db } from "./firebase-config";
+import { auth, db } from "./firebase-config";
+import _ from "lodash";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 import {
   doc,
   addDoc,
@@ -13,11 +20,48 @@ import {
 function App() {
   const [users, setUsers] = useState([]);
   const [newUser, setNewUser] = useState({ name: "", age: "" });
+  const [loginData, setLoginData] = useState({ email: "", password: "" });
+  const [registerData, setRegisterData] = useState({ email: "", password: "" });
   const userCollectionRef = collection(db, "users");
+  const [authUser, setAuthUser] = useState({});
 
-  const register = async () => {};
-  const login = async () => {};
-  const logout = async () => {};
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      setAuthUser(currentUser);
+    });
+  }, []);
+
+  useEffect(() => {
+    console.log("authUser ==> ", authUser);
+  }, [authUser]);
+
+  const register = async () => {
+    try {
+      const user = await createUserWithEmailAndPassword(
+        auth,
+        registerData.email,
+        registerData.password
+      );
+      console.log("user ==> ", user);
+    } catch (error) {
+      console.log("error ==> ", error.message);
+    }
+  };
+  const login = async () => {
+    try {
+      const user = await signInWithEmailAndPassword(
+        auth,
+        loginData.email,
+        loginData.password
+      );
+      console.log("user ==> ", user);
+    } catch (error) {
+      console.log("error ==> ", error.message);
+    }
+  };
+  const logout = async () => {
+    await signOut(auth);
+  };
 
   const createUser = async () => {
     await addDoc(userCollectionRef, newUser);
@@ -35,6 +79,7 @@ function App() {
     const subCollectionRef = collection(db, "users", id, "subCollection");
     await addDoc(subCollectionRef, { name: "Ankit Grin", age: 20 });
   };
+
   const renderUsers = (user, index) => {
     return (
       <div
@@ -81,37 +126,67 @@ function App() {
 
   return (
     <div className="App">
-      <>
-        <div>
-          <h3>Register User</h3>
-          <input placeholder="Email..." />
-          <input placeholder="Password..." />
-          <button>Create User</button>
-        </div>
-        <div>
-          <h3>Login</h3>
-          <input placeholder="Email..." />
-          <input placeholder="Password..." />
-          <button>Login</button>
-        </div>
-      </>
-      <>
-        <h3>Add User</h3>
-        <input
-          value={newUser.name}
-          placeholder="Name..."
-          onChange={(e) => setNewUser({ ...newUser, name: e.target.value })}
-        />
-        <input
-          value={newUser.age}
-          type="number"
-          placeholder="Age..."
-          onChange={(e) => setNewUser({ ...newUser, age: e.target.value })}
-        />
-        <button onClick={createUser}>Create User</button>
+      {authUser == null ? (
+        <>
+          <div>
+            <h3>Register User</h3>
+            <input
+              placeholder="Email..."
+              onChange={(e) =>
+                setRegisterData({ ...registerData, email: e.target.value })
+              }
+            />
+            <input
+              placeholder="Password..."
+              onChange={(e) =>
+                setRegisterData({ ...registerData, password: e.target.value })
+              }
+            />
+            <button onClick={register}>Create User</button>
+          </div>
+          <div>
+            <h3>Login</h3>
+            <input
+              placeholder="Email..."
+              onChange={(e) =>
+                setLoginData({ ...loginData, email: e.target.value })
+              }
+            />
+            <input
+              placeholder="Password..."
+              onChange={(e) =>
+                setLoginData({ ...loginData, password: e.target.value })
+              }
+            />
+            <button onClick={login}>Login</button>
+          </div>
+        </>
+      ) : _.isEmpty(authUser) ? (
+        <></>
+      ) : (
+        <>
+          <div>
+            <h3>User Logged In</h3>
+            {authUser?.email}
+            <button onClick={logout}>Sign Out</button>
+          </div>
+          <h3>Add User</h3>
+          <input
+            value={newUser.name}
+            placeholder="Name..."
+            onChange={(e) => setNewUser({ ...newUser, name: e.target.value })}
+          />
+          <input
+            value={newUser.age}
+            type="number"
+            placeholder="Age..."
+            onChange={(e) => setNewUser({ ...newUser, age: e.target.value })}
+          />
+          <button onClick={createUser}>Create User</button>
 
-        {users.map(renderUsers)}
-      </>
+          {users.map(renderUsers)}
+        </>
+      )}
     </div>
   );
 }
